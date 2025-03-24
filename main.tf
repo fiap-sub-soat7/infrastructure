01d5dad60882acf4d1895d545d5d7e332773b952
guinegreiros-dev <guilhermenegreiros12@gmail.com>
2025-03-24 18:58:40 -0300

    refactor: remove rds

--- main.tf
+++ main.tf
@@ -1,8 +1,6 @@
 module "t75-app" {
   source = "./resources"
   ACCOUNT_ID = var.ACCOUNT_ID
-  RDS_DEFAULT_PASS = var.RDS_DEFAULT_PASS
-  RDS_DEFAULT_USER = var.RDS_DEFAULT_USER
   REGION = var.REGION
 }
 
--- resources/vars.tf
+++ resources/vars.tf
@@ -6,10 +6,3 @@ variable "ACCOUNT_ID" {
   type = string
 }
 
-variable "RDS_DEFAULT_PASS" {
-  type = string
-}
-
-variable "RDS_DEFAULT_USER" {
-  type = string
-}
