5c5cb0ea83eb5f38a9873abab9235d3f8c13afda
guinegreiros-dev <guilhermenegreiros12@gmail.com>
2025-03-28 10:24:22 -0300

    simplify eks

--- resources/eks.tf
+++ resources/eks.tf
@@ -12,37 +12,16 @@ resource "aws_eks_cluster" "t75-eks_cluster" {
   access_config {
     authentication_mode = "API_AND_CONFIG_MAP"
   }
-
-  depends_on = [
-    aws_iam_role_policy_attachment.eks_cluster_policy,
-  ]
 }
 
 resource "aws_eks_access_policy_association" "t75-eks_access_policy" {
   cluster_name = aws_eks_cluster.t75-eks_cluster.name
   policy_arn = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
-  principal_arn = "arn:aws:iam::${var.ACCOUNT_ID}:role/t75-eks-cluster-role"
+  principal_arn = aws_iam_role.eks_cluster_role.arn
 
   access_scope {
     type = "cluster"
   }
-
-  depends_on = [aws_eks_access_entry.t75-eks_access_entry]
-}
-
-resource "aws_iam_role" "eks_node_group" {
-  name = "eks-node-group-role"
-
-  assume_role_policy = jsonencode({
-    Statement = [{
-      Action = "sts:AssumeRole"
-      Effect = "Allow"
-      Principal = {
-        Service = "ec2.amazonaws.com"
-      }
-    }]
-    Version = "2012-10-17"
-  })
 }
 
 resource "aws_eks_node_group" "t75-eks-node_group" {
@@ -63,34 +42,4 @@ resource "aws_eks_node_group" "t75-eks-node_group" {
   update_config {
     max_unavailable = 1
   }
-
-  ami_type       = "AL2_x86_64"  # Explicit AMI type
-  capacity_type  = "ON_DEMAND"   # More reliable than SPOT for initial setup
-  disk_size      = 20            # Minimum recommended size
-
-  labels = {
-    role = "worker"
-  }
-
-  taint {
-    key    = "dedicated"
-    value  = "worker"
-    effect = "NO_SCHEDULE"
-  }
-}
-
-resource "aws_eks_access_entry" "t75-eks_access_entry" {
-  cluster_name  = aws_eks_cluster.t75-eks_cluster.name
-  principal_arn = "arn:aws:iam::${var.ACCOUNT_ID}:role/t75-eks-cluster-role"
-  type          = "STANDARD"  # Or "EC2_LINUX"/"EC2_WINDOWS" if it was auto-created
-}
-
-data "tls_certificate" "eks_oidc" {
-  url = aws_eks_cluster.t75-eks_cluster.identity[0].oidc[0].issuer
-}
-
-resource "aws_iam_openid_connect_provider" "eks_oidc" {
-  client_id_list  = ["sts.amazonaws.com"]
-  thumbprint_list = [data.tls_certificate.eks_oidc.certificates[0].sha1_fingerprint]
-  url             = aws_eks_cluster.t75-eks_cluster.identity[0].oidc[0].issuer
 }
