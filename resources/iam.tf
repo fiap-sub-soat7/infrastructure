5953d21bf0f279caf2d3e30f840a1f9357e9bd3b
guinegreiros-dev <guilhermenegreiros12@gmail.com>
2025-03-28 15:51:19 -0300

    add quotes

--- resources/iam.tf
+++ resources/iam.tf
@@ -243,7 +243,7 @@ resource "aws_iam_role_policy_attachment" "alb_controller_attach" {
 
 resource "aws_eks_access_entry" "github_actions_user" {
   cluster_name  = aws_eks_cluster.t75-eks_cluster.name
-  principal_arn = arn:aws:iam::025066260361:user/github-actions-user
+  principal_arn = "arn:aws:iam::025066260361:user/github-actions-user"
   type          = "STANDARD"
 }
 
