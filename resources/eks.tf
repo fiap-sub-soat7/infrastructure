916f8ce37c76235731678f8760b6997afed6d2bd
guinegreiros-dev <guilhermenegreiros12@gmail.com>
2025-03-28 10:40:37 -0300

    add eks_cluster_role

--- resources/eks.tf
+++ resources/eks.tf
@@ -43,3 +43,21 @@ resource "aws_eks_node_group" "t75-eks-node_group" {
     max_unavailable = 1
   }
 }
+
+# EKS Cluster Role (for the control plane)
+resource "aws_iam_role" "eks_cluster_role" {
+  name = "t75-eks-cluster-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Action = "sts:AssumeRole",
+        Effect = "Allow",
+        Principal = {
+          Service = "eks.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
