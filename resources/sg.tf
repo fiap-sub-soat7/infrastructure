0bd73bb8a2ab0db511e43e34d346e6295af1a44d
guinegreiros-dev <guilhermenegreiros12@gmail.com>
2025-03-28 14:30:19 -0300

    update sg

--- resources/sg.tf
+++ resources/sg.tf
@@ -9,35 +9,6 @@ resource "aws_security_group" "t75-sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-  ingress {
-    from_port = 443
-    to_port = 443
-    protocol = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  # liberado para rds, TODO criar outro sg
-  ingress {
-    from_port = 3306 # mysql
-    to_port = 3306
-    protocol = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port = 5432 # postgress
-    to_port = 5432
-    protocol = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port = 0
-    to_port = 0
-    protocol = "-1" 
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
   # REQUIRED FOR EKS
   ingress {
     description = "Cluster API server"
